56b5817e4841cd29ba4963b7fd3b78f1283f78cd
tilacog <tilacog@gmail.com>
2021-08-10 22:27:56 -0300

    fix bug

--- src/main.rs
+++ src/main.rs
@@ -25,27 +25,24 @@ fn view(app: &App, _model: &Model, frame: Frame) {
     let draw = app.draw().scale_axes(vec3(SIZE, SIZE, 1.0));
     draw.background().color(BLACK);
     let size = 0.4;
-    draw_fractal(Vec2::ZERO, size, 1, &draw);
+    draw_fractal(Vec2::ZERO, size, 3, &draw);
     draw.to_frame(app, &frame).unwrap();
 }
 
 fn draw_fractal(position: Vec2, size: f32, depth: usize, draw: &Draw) {
-    // let draw = draw.translate(starting_point.extend(0.0));
     let sides = 3;
     let increment = 1.0 / sides as f32;
-
+    let scale = 0.5;
     for i in 0..sides {
         let fraction = i as f32 * increment;
         let angle = fraction;
-
         if depth > 0 {
-            let scale = 0.5;
             let new_size = size * scale;
-            let new_position = polar(angle, new_size);
+            let new_position = polar(angle, new_size) + position;
             draw_fractal(new_position, new_size, depth - 1, &draw);
         } else {
-            let pt1 = polar(angle, size) + position;
-            let pt2 = polar(angle + increment, size) + position;
+            let pt1 = position + polar(angle, size);
+            let pt2 = position + polar(angle + increment, size);
             draw.line().start(pt1).end(pt2).weight(0.002).color(WHITE);
         }
     }
