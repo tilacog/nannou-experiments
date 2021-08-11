a696379dd38beadfc12b9ee9440ec63f2f05be63
tilacog <tilacog@gmail.com>
2021-08-10 21:18:08 -0300

    normalization

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,6 @@
 use nannou::prelude::*;
 
-const WIDTH: f32 = 500.0;
-const HEIGHT: f32 = 500.0;
+const SIZE: f32 = 1_000.0;
 
 fn main() {
     nannou::app(model).update(update).run();
@@ -10,10 +9,9 @@ fn main() {
 struct Model {}
 
 fn model(app: &App) -> Model {
-    app.set_loop_mode(LoopMode::loop_once());
     let _window = app
         .new_window()
-        .size(WIDTH as u32, HEIGHT as u32)
+        .size(SIZE as u32, SIZE as u32)
         .view(view)
         .build()
         .expect("failed to build window");
@@ -24,7 +22,25 @@ fn model(app: &App) -> Model {
 fn update(_app: &App, _model: &mut Model, _update: Update) {}
 
 fn view(app: &App, _model: &Model, frame: Frame) {
-    let draw = app.draw();
+    let draw = app.draw().scale_axes(vec3(SIZE, SIZE, 1.0));
     draw.background().color(BLACK);
+
+    let circle_size = 1.0;
+    draw.ellipse()
+        .color(WHITE)
+        .w_h(circle_size, circle_size)
+        .resolution(100.0);
+
+    if app.elapsed_frames() % 60 == 0 {
+        dbg!(normalized_mouse(app));
+    }
+
     draw.to_frame(app, &frame).unwrap();
+}
+
+fn normalized_mouse(app: &App) -> Vec2 {
+    let half = SIZE * 0.5;
+    let mouse_x = map_range(app.mouse.x, -half, half, 0.0, 1.0);
+    let mouse_y = map_range(app.mouse.y, -half, half, 0.0, 1.0);
+    vec2(mouse_x, mouse_y)
 }
