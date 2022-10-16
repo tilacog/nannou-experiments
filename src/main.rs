a597d57fb12a8053ee657ed180ce0ca6833fd2d3
tilacog <tilacog@pop-os.localdomain>
2022-10-16 12:19:23 -0300

    refactor

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,46 @@
 use nannou::prelude::*;
 
-fn main() {
-    nannou::app(model).update(update).run();
+const RATIO: f64 = 1.4142;
+const SHORT: f64 = 500.0;
+const LONG: f64 = SHORT * RATIO;
+
+// * Model
+struct Model {
+    grid: Grid,
 }
 
-struct Model {}
+impl Model {
+    fn new(app: &App) -> Model {
+        app.set_loop_mode(LoopMode::loop_once());
+        let _window = app
+            .new_window()
+            .size(LONG as u32, SHORT as u32)
+            .view(view)
+            .build()
+            .expect("failed to build window");
 
-fn model(app: &App) -> Model {
-    app.set_loop_mode(LoopMode::loop_once());
-    let _window = app
-        .new_window()
-        .size(500, 500)
-        .view(view)
-        .build()
-        .expect("failed to build window");
+        Model { grid: Grid {} }
+    }
 
-    Model {}
+    fn draw(&self, draw: &Draw) {
+        draw.background().color(BLACK);
+        draw.ellipse().color(STEELBLUE);
+    }
 }
+
+// * Grid
+struct Grid {}
+
+// * core
 
 fn update(_app: &App, _model: &mut Model, _update: Update) {}
 
-fn view(app: &App, _model: &Model, frame: Frame) {
+fn view(app: &App, model: &Model, frame: Frame) {
     let draw = app.draw();
-    draw.background().color(BLACK);
+    model.draw(&draw);
     draw.to_frame(app, &frame).unwrap();
+}
+
+fn main() {
+    nannou::app(Model::new).update(update).run();
 }
