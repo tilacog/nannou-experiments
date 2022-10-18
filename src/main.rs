0e5af37f20af811cc08785a96246a133732c75a5
tilacog <tilacog@gmail.com>
2022-10-18 19:04:45 -0300

    clippy and use constants

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,24 @@
 use nannou::prelude::*;
 use rand::Rng;
-use std::{
-    collections::{HashSet, VecDeque},
-    iter::successors,
-};
+use std::{collections::VecDeque, f32::consts::SQRT_2, iter::successors};
 
-const RATIO: f32 = 1.4142;
+const RATIO: f32 = SQRT_2;
 const HEIGHT: f32 = 500.0;
 const WIDTH: f32 = HEIGHT * RATIO;
 const _MARGIN: f32 = HEIGHT / 10.0;
 
-const DOT_COLOR: Rgb<u8> = PLUM;
-const DOT_SIZE: f32 = 1.0;
-const LINE_COLOR: Rgb<u8> = STEELBLUE;
-const LINE_WEIGHT: f32 = 1.0;
+const LINE_WEIGHT: f32 = 1.125;
 
-const SQRT_3: f32 = 1.73205080757;
+const SQRT_3: f32 = 1.732_050_8;
 const SIDE_LENGTH: f32 = 80.0;
 const CIRCUMRADIUS: f32 = SIDE_LENGTH * SQRT_3 / 3.0;
 const INRADIUS: f32 = CIRCUMRADIUS / 2.0;
+
+/// Shoud be between 0 and 1
+const SURVIVAL_FACTOR: f32 = 0.90;
+const HUE_RADIAN_INCREASE: f32 = TAU / 12.0;
+/// Shoud be between 0 and 1
+const ALPHA_DECAY: f32 = 0.65;
 
 // * Model
 struct Model {
@@ -35,8 +35,19 @@ impl Model {
             .build()
             .expect("failed to build window");
 
+        let color = {
+            let hue: f32 = rand::thread_rng().gen();
+            nannou::color::hsla(hue, 0.75, 0.5, 1.0)
+        };
         Model {
-            triangle: Triangle::new(Point2::new(0.0, 0.0), INRADIUS, CIRCUMRADIUS, 0.0),
+            triangle: Triangle::new(
+                Point2::new(0.0, 0.0),
+                INRADIUS,
+                CIRCUMRADIUS,
+                0.0,
+                1.0,
+                color,
+            ),
         }
     }
 
@@ -46,7 +57,6 @@ impl Model {
     }
 
     fn draw_triangles(&self, draw: &Draw) {
-        let mut triangles_left = 10;
         let mut queue: VecDeque<Triangle> = VecDeque::new();
         queue.push_back(self.triangle.clone()); // initialize queue
         let mut rendered: Vec<Point2> = Vec::new();
@@ -55,11 +65,6 @@ impl Model {
             triangle.draw(draw);
             rendered.push(triangle.origin);
 
-            triangles_left -= 1;
-            if triangles_left == 0 {
-                break;
-            }
-
             // Don't enqueue new triangles queue if they are close (the same) as previous triangles
             'outer: for new_triangle in triangle.project() {
                 for existing in &rendered {
@@ -67,8 +72,8 @@ impl Model {
                         continue 'outer;
                     }
                 }
-                // randomize enqueing of new triangles
-                if rand::thread_rng().gen_bool(0.6) {
+                // kill some triangles before they spawn
+                if rand::thread_rng().gen_bool(new_triangle.sibling_survival_rate as f64) {
                     queue.push_back(new_triangle);
                 }
             }
@@ -83,6 +88,8 @@ struct Triangle {
     inradius: f32,
     circumradius: f32,
     orientation: f32,
+    sibling_survival_rate: f32,
+    color: Hsla,
 }
 
 impl std::fmt::Debug for Triangle {
@@ -94,20 +101,25 @@ impl std::fmt::Debug for Triangle {
 }
 
 impl Triangle {
-    fn new(origin: Point2, inradius: f32, circumradius: f32, orientation: f32) -> Self {
+    fn new(
+        origin: Point2,
+        inradius: f32,
+        circumradius: f32,
+        orientation: f32,
+        sibling_survival_rate: f32,
+        color: Hsla,
+    ) -> Self {
         Self {
             origin,
             inradius,
             circumradius,
             orientation,
+            sibling_survival_rate,
+            color,
         }
     }
 
-    fn draw(&self, draw: &Draw) {
-        self.draw_triangle(draw);
-    }
-
-    fn stroke(a: Point2, b: Point2, color: Rgb<u8>, draw: &Draw) {
+    fn stroke(a: Point2, b: Point2, color: Hsla, draw: &Draw) {
         draw.line().start(a).end(b).weight(LINE_WEIGHT).color(color);
     }
 
@@ -121,25 +133,18 @@ impl Triangle {
             })
             .collect()
     }
-    fn draw_triangle(&self, draw: &Draw) {
+    fn draw(&self, draw: &Draw) {
         let points = Self::triangle_points(self.origin, self.orientation, self.circumradius);
 
         // draw sides
-        Self::stroke(points[0], points[1], LINE_COLOR, &draw);
-        Self::stroke(points[1], points[2], LINE_COLOR, &draw);
-        Self::stroke(points[2], points[0], LINE_COLOR, &draw);
-
-        // draw vertices
-        for point in &points {
-            draw.ellipse()
-                .color(DOT_COLOR)
-                .w_h(DOT_SIZE, DOT_SIZE)
-                .xy(*point);
-        }
+        Self::stroke(points[0], points[1], self.color, draw);
+        Self::stroke(points[1], points[2], self.color, draw);
+        Self::stroke(points[2], points[0], self.color, draw);
     }
     fn project(&self) -> impl Iterator<Item = Self> + '_ {
         const ONE_THIRD_ROTATION: f32 = TAU / 3.0;
         const ONE_SIXTH__ROTATION: f32 = TAU / 6.0;
+
         // rotate by 30 degrees and then take three one-third turns...
         successors(Some(self.orientation + ONE_SIXTH__ROTATION), |radians| {
             Some(radians + ONE_THIRD_ROTATION)
@@ -152,7 +157,26 @@ impl Triangle {
             let x = orientation.sin() * radius;
             let y = orientation.cos() * radius;
             let origin = self.origin + pt2(x, y);
-            Triangle::new(origin, self.inradius, self.circumradius, orientation)
+
+            let color = {
+                let (hue, saturation, lightness, alpha) = self.color.into_components();
+                let hue = {
+                    let radians = hue.to_radians();
+                    let updated = radians + HUE_RADIAN_INCREASE;
+                    nannou::color::RgbHue::from_radians(updated)
+                };
+                let alpha = alpha * ALPHA_DECAY;
+                nannou::color::Hsla::new(hue, saturation, lightness, alpha)
+            };
+
+            Triangle::new(
+                origin,
+                self.inradius,
+                self.circumradius,
+                orientation,
+                self.sibling_survival_rate * SURVIVAL_FACTOR,
+                color,
+            )
         })
     }
 }
